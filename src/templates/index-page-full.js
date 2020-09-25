34ee9e6fae332740b0ee8f103f023210d5247f97
zolbayars <zolube@gmail.com>
2020-09-25 09:40:24 +0800

    edit: remove landing page

--- src/templates/index-page-full.js
+++ src/templates/index-page-full.js
@@ -126,10 +126,68 @@ IndexPageTemplate.propTypes = {
   }),
 }
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
   return (
-      <h1>Work remotely from Asia!</h1>
+    <Layout>
+      <IndexPageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        subheading={frontmatter.subheading}
+        mainpitch={frontmatter.mainpitch}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
+      />
+    </Layout>
   )
 }
 
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
 export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        heading
+        subheading
+        mainpitch {
+          title
+          description
+        }
+        description
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+      }
+    }
+  }
+`
