f5c09393b369317cd340641ced0f7cf66c91bac1
zolbayars <zolube@gmail.com>
2020-12-19 09:44:24 +0800

    feat: custom 404 and new banner

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/common/Layout"
+
+export default function NotFound() {
+  return (
+    <>
+      <Layout>
+        <div className="container">
+          <h1>404</h1>
+          <p>Go back to the <a href="/">gereesee.com</a></p>
+        </div>
+      </Layout>
+    </>
+  )
+}
